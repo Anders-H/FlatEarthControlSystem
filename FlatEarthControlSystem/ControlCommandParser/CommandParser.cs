bfada9f1545f20b66c046bacefaf46bb0465001f
Anders-H <anders@winsoft.se>
2020-08-27 18:33:00 +0200

    Fixed compile errors from command parser result change.

--- FlatEarthControlSystem/ControlCommandParser/CommandParser.cs
+++ FlatEarthControlSystem/ControlCommandParser/CommandParser.cs
@@ -10,7 +10,12 @@ namespace FlatEarthControlSystem.ControlCommandParser
         private readonly Room _room;
         private readonly string _phrase;
         private readonly List<string> _parts;
-        
+
+        private const string CommandShow = "SHOW";
+        private const string SpecialNounInventory = "INVENTORY";
+        private const string SpecialNounExits = "EXITS";
+        private const string CommandGo = "GO";
+
         public CommandParser(Room room, string phrase)
         {
             _room = room;
@@ -22,47 +27,63 @@ namespace FlatEarthControlSystem.ControlCommandParser
         {
             if (string.IsNullOrWhiteSpace(_phrase) || _parts.Count <= 0)
                 return CommandParserResult.CreateFailResult("");
+
             var command = _parts.Pop();
+
+            //Check if shortcut or single word command is used.
             switch (command)
             {
-                case "INVENTORY":
+                case SpecialNounInventory:
                 case "INV":
                     return CommandParserResult.CreateSuccessResult(
-                        new SuggestedCommand("SHOW", "INVENTORY"),
-                        PreProcessorIntention.Inventory);
+                        new SuggestedCommand(CommandShow, "INVENTORY"),
+                        PreProcessorIntention.Inventory,
+                        CommandShow, SpecialNounInventory, "");
                 case "N":
                 case "NORTH":
                     return CommandParserResult.CreateSuccessResult(
-                        new SuggestedCommand("GO", "NORTH"),
-                        PreProcessorIntention.Move);
+                        new SuggestedCommand(CommandGo, "NORTH"),
+                        PreProcessorIntention.Move,
+                        CommandGo, "NORTH", "");
                 case "E":
                 case "EAST":
                     return CommandParserResult.CreateSuccessResult(
-                        new SuggestedCommand("GO", "EAST"),
-                        PreProcessorIntention.Move);
+                        new SuggestedCommand(CommandGo, "EAST"),
+                        PreProcessorIntention.Move,
+                        CommandGo, "EAST", "");
                 case "S":
                 case "SOUTH":
                     return CommandParserResult.CreateSuccessResult(
-                        new SuggestedCommand("GO", "SOUTH"),
-                        PreProcessorIntention.Move);
+                        new SuggestedCommand(CommandGo, "SOUTH"),
+                        PreProcessorIntention.Move,
+                        CommandGo, "SOUTH", "");
                 case "W":
                 case "WEST":
                     return CommandParserResult.CreateSuccessResult(
-                        new SuggestedCommand("GO", "SOUTH"),
-                        PreProcessorIntention.Move);
-                case "EXITS":
+                        new SuggestedCommand(CommandGo, "WEST"),
+                        PreProcessorIntention.Move,
+                        CommandGo, "WEST", "");
+                case SpecialNounExits:
                     return CommandParserResult.CreateSuccessResult(
-                        new SuggestedCommand("SHOW", "EXITS"),
-                        PreProcessorIntention.Exits);
+                        new SuggestedCommand(CommandShow, SpecialNounExits),
+                        PreProcessorIntention.Exits,
+                        CommandShow, SpecialNounExits, "");
             }
-            if (_parts.Count <= 0)
-                return CommandParserResult.CreateFailResult();
-            switch (command)
+
+            switch (_parts.Count)
             {
-                case "GO":
-                    return Go();
-                case "SHOW":
-                    return Show();
+                case 0:
+                    return CommandParserResult.CreateFailResult();
+                case 1:
+                    switch (command)
+                    {
+                        case CommandGo:
+                            return Go();
+                        case CommandShow:
+                            return Show();
+                        default:
+                            return CommandParserResult.CreateFailResult();
+                    }
                 default:
                     return CommandParserResult.CreateFailResult();
             }
@@ -77,8 +98,9 @@ namespace FlatEarthControlSystem.ControlCommandParser
             return exit == null
                 ? CommandParserResult.CreateFailResult("CAN'T GO THAT WAY.")
                 : CommandParserResult.CreateSuccessResult(
-                    new SuggestedCommand("GO", exit.ToString()),
-                    PreProcessorIntention.Move);
+                    new SuggestedCommand(CommandGo, exit.ToString()),
+                    PreProcessorIntention.Move,
+                    CommandGo, direction, "");
         }
 
         private CommandParserResult Show()
@@ -89,15 +111,17 @@ namespace FlatEarthControlSystem.ControlCommandParser
                 return CommandParserResult.CreateFailResult(error);
             switch (show)
             {
-                case "INVENTORY":
+                case SpecialNounInventory:
                 case "INV":
                     return CommandParserResult.CreateSuccessResult(
-                        new SuggestedCommand("SHOW", "INVENTORY"),
-                        PreProcessorIntention.Inventory);
-                case "EXITS":
+                        new SuggestedCommand(CommandShow, SpecialNounInventory),
+                        PreProcessorIntention.Inventory,
+                        CommandShow, SpecialNounInventory, "");
+                case SpecialNounExits:
                     return CommandParserResult.CreateSuccessResult(
-                        new SuggestedCommand("SHOW", "EXITS"),
-                        PreProcessorIntention.Exits);
+                        new SuggestedCommand(CommandShow, SpecialNounExits),
+                        PreProcessorIntention.Exits,
+                        CommandShow, SpecialNounExits, "");
             }
             return CommandParserResult.CreateFailResult(error);
         }
