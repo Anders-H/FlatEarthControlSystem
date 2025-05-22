a1de59d446519474d450bcc78fdccaed3d1d7072
Anders Hesselbom <anders@winsoft.se>
2025-05-22 17:36:17 +0200

    Basic typing in the input control.

--- AdventureControlLibrary/TextInputControl.cs
+++ AdventureControlLibrary/TextInputControl.cs
@@ -1,5 +1,4 @@
 ﻿using System.Drawing.Drawing2D;
-using System.Security.Cryptography;
 using Matrix = CharacterMatrix.Matrix;
 
 namespace AdventureControlLibrary;
@@ -7,6 +6,7 @@ namespace AdventureControlLibrary;
 public partial class TextInputControl : UserControl
 {
     public event CommandEnteredDelegate CommandEntered;
+    private readonly TerminalFont _font;
     private readonly Bitmap _bitmap;
     private readonly Matrix _characterMatrix;
     private bool _cursorVisible;
@@ -21,6 +21,7 @@ public partial class TextInputControl : UserControl
 
     public TextInputControl()
     {
+        _font = new TerminalFont();
         CursorX = 0;
         _bitmap = new Bitmap(PixelsWidth, PixelsHeight);
         _characterMatrix = new Matrix(CharactersWidth, CharactersHeight);
@@ -63,7 +64,19 @@ public partial class TextInputControl : UserControl
             {
                 var pixelX = characterX * CharacterWidth + x;
                 var pixelY = characterY * CharacterHeight + y;
-                _bitmap.SetPixel(pixelX, pixelY, cursorVisible ? InputColor : BackgroundColor);
+                var c = _characterMatrix.GetAt(characterX, characterY);
+                _ = _font.TryGetValue(c, out var matrix);
+                var color = InputColor;
+
+                if (cursorVisible)
+                {
+                    _bitmap.SetPixel(pixelX, pixelY, matrix.Pixels[x, y] ? BackgroundColor : color);
+                }
+                else
+                {
+                    _bitmap.SetPixel(pixelX, pixelY, matrix.Pixels[x, y] ? color : BackgroundColor);
+                }
+
             }
         }
     }
@@ -84,6 +97,33 @@ public partial class TextInputControl : UserControl
 
                 ResetBlink();
                 break;
+            case Keys.Back:
+                // TODO!!!!
+                break;
+            case Keys.Delete:
+                // TODO!!!!
+                break;
+            case Keys.Enter:
+                // TODO!!!!
+                break;
+            case Keys.Insert:
+                DoInsert();
+                break;
+            case Keys.Space:
+            case Keys.Oemcomma:
+            case Keys.OemPeriod:
+            case Keys.OemQuestion:
+            case Keys.OemMinus:
+            case Keys.D0:
+            case Keys.D1:
+            case Keys.D2:
+            case Keys.D3:
+            case Keys.D4:
+            case Keys.D5:
+            case Keys.D6:
+            case Keys.D7:
+            case Keys.D8:
+            case Keys.D9:
             case Keys.A:
             case Keys.B:
             case Keys.C:
@@ -117,12 +157,106 @@ public partial class TextInputControl : UserControl
 
     private char GetCharacter(Keys key)
     {
-
+        switch (key)
+        {
+            case Keys.Oemcomma:
+                return ',';
+            case Keys.OemPeriod:
+                return '.';
+            case Keys.OemQuestion:
+                return '?';
+            case Keys.OemMinus:
+                return '-';
+            case Keys.D0:
+                return '0';
+            case Keys.D1:
+                return '1';
+            case Keys.D2:
+                return '2';
+            case Keys.D3:
+                return '3';
+            case Keys.D4:
+                return '4';
+            case Keys.D5:
+                return '5';
+            case Keys.D6:
+                return '6';
+            case Keys.D7:
+                return '7';
+            case Keys.D8:
+                return '8';
+            case Keys.D9:
+                return '9';
+            case Keys.A:
+                return 'A';
+            case Keys.B:
+                return 'B';
+            case Keys.C:
+                return 'C';
+            case Keys.D:
+                return 'D';
+            case Keys.E:
+                return 'E';
+            case Keys.F:
+                return 'F';
+            case Keys.G:
+                return 'G';
+            case Keys.H:
+                return 'H';
+            case Keys.I:
+                return 'I';
+            case Keys.J:
+                return 'J';
+            case Keys.K:
+                return 'K';
+            case Keys.L:
+                return 'L';
+            case Keys.M:
+                return 'M';
+            case Keys.N:
+                return 'N';
+            case Keys.O:
+                return 'O';
+            case Keys.P:
+                return 'P';
+            case Keys.Q:
+                return 'Q';
+            case Keys.R:
+                return 'R';
+            case Keys.S:
+                return 'S';
+            case Keys.T:
+                return 'T';
+            case Keys.U:
+                return 'U';
+            case Keys.V:
+                return 'V';
+            case Keys.W:
+                return 'W';
+            case Keys.X:
+                return 'X';
+            case Keys.Y:
+                return 'Y';
+            case Keys.Z:
+                return 'Z';
+            default:
+                return ' ';
+        }
     }
 
     private void TypeCharacter(char character)
     {
+        DoInsert();
+        _characterMatrix.SetAt(CursorX, CursorY, character);
 
+        if (CursorX < CharactersWidth - 1)
+            CursorX++;
+
+        ResetBlink();
+    }
+
+    private void DoInsert()
+    {
     }
 
     private void ResetBlink()
